6710cb7bdf957a7e1d8315309d38536ab64f005f
Lina <linadiazbejar@gmail.com>
2023-11-15 15:53:13 -0500

    new

--- AGRICULTURA/REVISION_DATOS_MINISTERIO_AGRICOLA_BENEFICIARIO.R
+++ AGRICULTURA/REVISION_DATOS_MINISTERIO_AGRICOLA_BENEFICIARIO.R
@@ -256,17 +256,6 @@ Ben$RazonSocialContratista <- Ben$RazonSocialContratista %>%
     `FUNDACION PARA LA PROSPERIDAD DE LAS COMUNIDADES MAS VULNERABLES` = c("FUNDACION PARA LA PROSPERIDAD DE LAS COMUNIDADES MAS VULNERABLES", "FUNDACION PARA LA PROSPERDAD DE LAS COMUNIDADES MAS VULNERABLES")
   ) %>% as.character() %>%  toupper()
 
-
-### REEMPLAZAR F POR NA
-Ben$NoConvenio <-Ben$NoConvenio %>%
-  as.character() %>%
-  ifelse(. %in% c("F"), NA, .) %>%  toupper()
-
-### LIMITAR EDADES DE 0 A 118 AÑOS, LAS DEMAS CONVERTIRLAS A NA
-
-Ben$Edad[Ben$Edad>118|Ben$Edad <0] <- NA
-
-
 ############ Pasar todo a mayúscula
 # Bucle a través de cada columna en el dataframe
 for (column in names(Ben)) {
@@ -277,14 +266,90 @@ for (column in names(Ben)) {
   }
 }
 
+### REEMPLAZAR F POR NA
+Ben$NoConvenio <-Ben$NoConvenio %>%
+  as.character() %>%
+  ifelse(. %in% c("F"), NA, .) %>%  toupper()
+
+### LIMITAR EDADES DE 0 A 118 AÑOS, LAS DEMAS CONVERTIRLAS A NA
+
+Ben$Edad[Ben$Edad>118|Ben$Edad <0] <- NA
+
+#### REMPLAZAR N/A en NA en la columna Númerodehectareascultivo
+Ben$NoHectareaCultivo <-Ben$NoHectareaCultivo %>%
+  as.numeric() %>%
+  ifelse(. %in% c("N/A"), NA, .) %>%  toupper()
+
+### CONVertir variables de hectareas a una variable a númerica
+
+Ben$NoHectareaCultivo <- as.numeric(Ben$NoHectareaCultivo)
+Ben$NoHectareaPredio <- as.numeric(Ben$NoHectareaPredio)
+Ben$NoHectareaCultivo <- ifelse(Ben$NoHectareaCultivo > 900000, Ben$NoHectareaCultivo / 10000, Ben$NoHectareaCultivo)
+Ben$NoHectareaPredio <- ifelse(Ben$NoHectareaPredio > 1000000, Ben$NoHectareaPredio / 10000, Ben$NoHectareaPredio)
+
+####### descriptivos para la variables valor de beneficio ####### 
+options(scipen=999)
+summary(Ben$ValorBeneficio)
+
+p <- seq(0.01, 0.99, 0.25)
+percentiles <- quantile(Ben$ValorBeneficio, p,na.rm=TRUE)
+head(percentiles)
+
+######## HISTOGRAMA 
+hist(Ben$ValorBeneficio, 
+     main = "Histogram con todos los valores",
+     xlab = "Values",
+     ylab = "Frequency",
+     col = "skyblue",
+     border = "black",
+     breaks = 20)
+
+#### Variable con rango de  valor beneficios 
+Ben$RangoBeneficio <- ifelse(Ben$ValorBeneficio> 5000000000,"Mas grandes beneficios","Beneficios hasta 5000 millones")
+
+table(Ben$RangoBeneficio, useNA = "ifany")
+
+#### Histograma valores menores a 10 millones
+filtered_bene10M <- Ben[Ben$ValorBeneficio<=10000000, ]
+
+summary(filtered_bene10M$ValorBeneficio)
+
+p <- seq(0.01, 0.99, 0.25)
+percentiles <- quantile(filtered_bene10M$ValorBeneficio, p,na.rm=TRUE)
+head(percentiles)
+
+hist(filtered_bene500M$ValorBeneficio, 
+     main = "Histograma con beneficios menores a 10.000.000",
+     xlab = "Values",
+     ylab = "Frequency",
+     col = "skyblue",
+     border = "black",
+     breaks = 10)
+
+
+#### 
+Ben$RangoBeneficio1 <- cut(Ben$ValorBeneficio,c(-1, 500000, 1000000, 3000000,5000000, 10000000, 20000000, 40000000, 100000000, 300000000, 500000000, 5000000000, Inf),
+                                    labels = c( "0 a 500.000", "500.000 a 1.000.000", "1.000.000 a 3.000.000","3.000.000 a 5.000.000", "5.000.000 a 10.000.000", "10.000.000 a 20.000.000", "20.000.000 a 40.000.000",
+                                                "40.000.000 a 100.000.000", "100.000.000 a 300.000.000", "300.000.000 a 500.000.000", "500.000.000 a 5000.000.000", "Mayor a 5.000.000.000"))
+
+table_counts<-table(Ben$RangoBeneficio1, useNA = "ifany")
+# Convert counts to percentages
+table_percentages <- prop.table(table_counts) * 100
+
+
 ############################### Agrupar información de columna Observaciones #########################################
 
+### eliminar tildes de la columna observaciones
+Ben$Observaciones<- iconv(Ben$Observaciones, to = "ASCII//TRANSLIT")
+
+
+
 # Listado de palabras clave
+
 palabras_clave <- c("AGUA POTABLE", "AGUACATE", "AVICOLA","CACAO", "LULO", 
-                    "PRODUCCION Y COMERCIALIZACION DE YUCA Y MAIZ","ESTABLECIMEINTO Y COMERCIALIZACION DE CUTIVOS ",	
+                    "PRODUCCION Y COMERCIALIZACION DE YUCA Y MAIZ","ESTABLECIMEINTO Y COMERCIALIZACION DE CUTIVOS ","PLATANO BUENAVISTA MUJERES CAFETERAS",	"TIENDA MOVIL TIPO CAFETERIA","CAFETERIA","CAFE INTERNET Y SERVICIO FOTOGRAFICO"," CAFETERIA ",
                     "SIEMBRA YUCA, PLATANO Y MAIZ","CAÑA","SACHA INCHI","TRUCHA","TOMATE","PANELA","PAPA","LECHE","LIMÓN","PORCINOS","PORCICOLA"," PORCICULTURA",
-                    "MAIZ","PRODUCCION Y COMERCIALIZACION DE YUCA Y MAIZ","MARACUYA","MIEL","MODISTERIA","MORA","YUCA","VIVERO",
-                    "VENTA DE ANIMAL DE ABASTO","UCHUVA","TURISMO","TRUCHA","TRILLADORA ARROZ","TRAPICHE","YUCA","CAFE","TRANSFORMACIÓN DE PLATANO"," TILAPIA","BELLEZA"," RESTAURANTE")
+                    "MAIZ","PRODUCCION Y COMERCIALIZACION DE YUCA Y MAIZ","MARACUYA","MIEL","MODISTERIA","MORA","YUCA","VIVERO","VENTA DE ANIMAL DE ABASTO","UCHUVA","TURISMO","TRUCHA","TRILLADORA ARROZ","TRAPICHE","YUCA","CAFE","TRANSFORMACION DE PLATANO"," TILAPIA","BELLEZA"," RESTAURANTE","HARINA DE BANANO Y TE DE CASCARA DE CAFE")
 
 # Función para buscar la palabra clave en el texto
 buscar_palabra_clave <- function(texto, palabras_clave) {
@@ -312,14 +377,14 @@ table(Ben$Observaciones, useNA = "ifany")
 # NitContratista (cambiar Null a NA) (ok)
 # RazonSocialContratista Pasar a mayúscula y unificar terminos (Cambiar Null por NA) (OK)
 # Edad (dejar solo las personas entre 10 y 100 años, el resto pasar a NA) (OK)
-# Correo (Pasar a Mayúsculas, dejar solo los objetos con @)
-# TelefonoContacto (Dejar solo los números de 7 y 10 dígitos)
+# Correo (Pasar a Mayúsculas, dejar solo los objetos con @) (pendiente)
+# TelefonoContacto (Dejar solo los números de 7 y 10 dígitos) (pendiente)
 # NombrePredio (Poner en Mayúsculas) (OK)
 # MunicipioPDET (NULL y NA poner como dato faltante) (OK)
-# Observaciones (Quitar NULL, poner todo en mayúscula quitar acentos, revisar si algo se puede agrupar)
-## Númerodehectareascultivo (remover valores atipicos de hectareas, quitar valores N/A, los valores superiores a 900 mil están en mt2 comparando hectareas predio convertir/ 100.000)
-## NoHectareaPredio (transforma los valores mayores a un millon que posiblemente estén digitados en mt2s y transformarlo en hectareas)
-#### Valor de beneficio (revisar la distribución de los datos y tratar de eliminar valores atipicos que no tengan sentido)
+# Observaciones (Quitar NULL, poner todo en mayúscula quitar acentos, revisar si algo se puede agrupar)(en proceso)
+## Númerodehectareascultivo (remover valores atipicos de hectareas, quitar valores N/A, los valores superiores a 900 mil están en mt2 comparando hectareas predio convertir/ 100.000) (OK)
+## NoHectareaPredio (transforma los valores mayores a un millon que posiblemente estén digitados en mt2s y transformarlo en hectareas, hay dos valores atipicos en 6400 hectareas se dejan tal cual) (OK)
+#### Valor de beneficio (revisar la distribución de los datos y tratar de eliminar valores atipicos que no tengan sentido, no se elimina ningún dato por ahora) (OK)
 
 
 
